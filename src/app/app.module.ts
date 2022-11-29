6d7abf2774055af10c801453a9a852b850622c35
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-30 01:03:20 +0530

    view post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddPostComponent } from './add-post/add-post.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { HttpClientModule } from "@angular/common/http"
+import { RouterModule, Routes } from '@angular/router';
+
+const myroute:Routes=[
+  {
+    path:"",
+    component:AddPostComponent
+  },
+  {
+    path:"view",
+    component:ViewPostComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddPostComponent
+    AddPostComponent,
+    ViewPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
